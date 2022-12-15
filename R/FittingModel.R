a355736d235cb241198fca3851dfa2b7964dd9e9
Wiebachj <jwiebac@mdc-berlin.net>
2022-12-15 21:50:44 +0100

    report outlier before and after model selection using standardized residuals

--- R/FittingModel.R
+++ R/FittingModel.R
@@ -16,7 +16,7 @@ chooseModel <- function(dat,
                         model=c("logistic", "linear", "quadratic"),
                         SRES = 2){
 
-  setorder(dat,DilutionPoint)[!is.na(get(tidyselect::all_of(y)))]
+  dat <- setorder(dat,DilutionPoint)[!is.na(get(tidyselect::all_of(y)))]
   dat$outlier <- FALSE
 
   if ("logistic" %in% model) {
@@ -26,6 +26,7 @@ chooseModel <- function(dat,
       datOut[[y]][which(residuals(logistic)/sd(residuals(logistic)) > SRES)] <- NA
       logisticOut <- drc::drm(get(tidyselect::all_of(y)) ~ get(x), fct = drc::L.3(), data = datOut)
       suppressWarnings(aic <- AIC(logistic, logisticOut))
+      aic$AIC[is.infinite(aic$AIC)] <- -1000
       logistic1 <- get(row.names(aic)[which(aic$AIC %in% min(aic$AIC))])
     } else{
       logistic1 <- logistic
@@ -43,6 +44,7 @@ chooseModel <- function(dat,
       datOut[[y]][which(residuals(linear)/sd(residuals(linear)) > SRES)] <- NA
       linearOut <- lm(get(tidyselect::all_of(y)) ~ get(x), data = datOut)
       suppressWarnings(aic <- AIC(linear, linearOut))
+      aic$AIC[is.infinite(aic$AIC)] <- -1000
       linear1 <- get(row.names(aic)[which(aic$AIC %in% min(aic$AIC))])
     } else{
       linear1 <- linear
@@ -59,6 +61,7 @@ chooseModel <- function(dat,
       datOut[[y]][which(residuals(quadratic)/sd(residuals(quadratic)) > SRES)] <- NA
       quadraticOut <- lm(get(tidyselect::all_of(y)) ~ poly(get(x), 2, raw = TRUE), data = datOut)
       suppressWarnings(aic <- AIC(quadratic, quadraticOut))
+      aic$AIC[is.infinite(aic$AIC)] <- -1000
       quadratic1 <- get(row.names(aic)[which(aic$AIC %in% min(aic$AIC))])
     } else{
       quadratic1 <- quadratic
@@ -74,9 +77,12 @@ chooseModel <- function(dat,
   # select Model by using akaike information criterium
 
   suppressWarnings(aic <- AIC(linear1, logistic1, quadratic1))
+  aic$AIC[is.infinite(aic$AIC)] <- -1000
   ModelName <- row.names(aic)[which(aic$AIC %in% min(aic$AIC))]
+  if ("linear1" %in% ModelName) {ModelName = "linear1"} else if (all(c("logistic1", "quadratic1") %in% ModelName)) {ModelName = "logistic1"}  # if same correlation
 
   dat$outlier[which(residuals(get(gsub(pattern = "1", x = ModelName, replacement = "")))/sd(residuals(get(gsub(pattern = "1", x = ModelName, replacement = "")))) > SRES)] <- TRUE
+  dat$outlier[as.integer(names(which(residuals(get(ModelName))/sd(residuals(get(ModelName))) > SRES)))] <- TRUE
 
 
   ## calculate correlation
@@ -89,10 +95,10 @@ chooseModel <- function(dat,
   #rm(list = c("logistic", "linear", "quadratic")[!c("logistic", "linear", "quadratic") %in% substr(cor.max,5, 100 )])
 
 
-  if ("linear" %in% ModelName) {ModelName = "linear"} else if (all(c("logistic", "quadratic") %in% ModelName)) {ModelName = "logistic"}  # if same correlation
+
   Model <- ModelName
 
-  Model = list("fit" = fitted(get(Model)), "coefficients" = coef(get(Model)), "residuals" = residuals(get(Model)))
+  Model = list("fit" = fitted(get(Model)), "coefficients" = coef(get(Model)), "residuals" = residuals(get(Model))/sd(residuals(get(Model))))
 
 
 
