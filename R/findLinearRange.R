a049d6a6ef16c1b3174fda6fcd627eda07e4ce59
Wiebachj <jwiebac@mdc-berlin.net>
2023-03-16 14:17:34 +0100

    new function to determine the positive association between x and y within one dilution curve

--- R/findLinearRange.R
+++ R/findLinearRange.R
@@ -35,21 +35,7 @@ findLinearRange <- function(dats, x="DilutionPoint", y = "IntensityNorm",  sd_re
   linearRange <- lm(dat[[y]] ~ dat[[x]], weights = we)
   ablineIntensity <- fitted(linearRange)
 
-  # newx <- seq(min(dat[[x]]), max(dat[[x]]), length.out=nrow(dat))
-  # newdata = data.frame(x = newx)
-  # names(newdata) = x
-  # preds <- predict(linearRange, newdata, interval = 'confidence', weights = we)
-  #
-  # limitdown <- preds[ ,2]
-  # limitup <- preds[ ,3]
-###use absolute percentage
-  #confint <- linearRange$coefficients[1]*res
-  #confint <- max(dat[[y]])/100*res
-### use scaled interval
-  #scalefit <- scale(ablineIntensity)
-  #scalefit <- scalefit*res/100
-  #fact <- ablineIntensity[1]*res/100/scalefit[1]
-###use residuals
+  ###use residuals
 
   sd_residuals <- abs(sd_res_factor*sd(residuals(linearRange)[which(abs(residuals(linearRange)) < 1)]))
   if(sd_residuals < 1) sd_residuals <- 1
@@ -57,64 +43,9 @@ findLinearRange <- function(dats, x="DilutionPoint", y = "IntensityNorm",  sd_re
   lr <- abs(residuals(linearRange)) < ceiling(sd_residuals*10)/10
 
 
-
-
-
-
-  #confi <- abs(scalefit*fact)
-
-
-
-
-  #confi <- abs(ablineIntensity/100)*res
-
-
-
-
-
-
-
-  #if(all(ablineIntensity/100*(100 + res) >= ablineIntensity)){
-  # limit1 <- ablineIntensity + confi
-  # limit2 <- ablineIntensity - confi
-  #
-  # if(limit1[1] < limit2[1]){
-  #   limitdown <- limit1
-  #   limitup <- limit2
-  # } else{
-  #   limitdown <- limit2
-  #   limitup <- limit1
-  # }
-  #limitup <- (linearRange$coefficients[1] + confint) + linearRange$coefficients[2]*dat[[x]]
-  #limitdown <- (linearRange$coefficients[1] - confint) + linearRange$coefficients[2]*dat[[x]]
-  #} else{
-
-  #limitup <- ablineIntensity - confi
-  #limitdown <- ablineIntensity + confi
-  #limitup <- (linearRange$coefficients[1] - confint) + linearRange$coefficients[2]*dat[[x]]
-  #limitdown <- (linearRange$coefficients[1] + confint) + linearRange$coefficients[2]*dat[[x]]
-
-  #}
-
-  # consNDX <- rle(data.table::between(x = dat[[y]], lower = floor(limitdown), upper = ceiling(limitup), NAbounds = NA, check = T))
-
    consNDX <- rle(lr)
 
-
-
-  #rm(linearRange)
-  #ndx <- which(abs((dat[[y]] - ablineIntensity) /max(abs(dat[[y]] - ablineIntensity))) < res)
-  #consNDX <- rle(round(abs((dat[[y]] - ablineIntensity) /max(abs(dat[[y]] - ablineIntensity))), digits = 2) <= res)
-
-
   consNDX$position <- cumsum(consNDX$length)
-
-
-
-  # dat <- setorder(dplyr::full_join(dat, dats[!IDintern %in% dat$IDintern]), DilutionPoint)
-  # dat$color[dat[[outlierName]] %in% TRUE] <- "red"
-  # dat$Comment[dat[[outlierName]] %in% TRUE] <- paste0(dat$Comment[dat[[outlierName]] %in% TRUE], "_Outlier",abbr)
-  # dat$Comment[dat[[outlierName]] %in% FALSE] <- paste0(dat$Comment[dat[[outlierName]] %in% FALSE], "_NoOutlier",abbr)
 
 
   if(any(consNDX$length[which(consNDX$values %in% TRUE)]>= min_feature) & any(consNDX$position[consNDX$values %in% TRUE] >= int50)){
@@ -178,7 +109,7 @@ findLinearRange <- function(dats, x="DilutionPoint", y = "IntensityNorm",  sd_re
         dat$color[dat$IsLinear %in% FALSE] <- "black"
 
         tmpGroup$LRStart = dat$DilutionPoint[dat$IsLinear %in% TRUE][1]
-        tmpGroup$LRStartY = ldat$Y[dat$IsLinear %in% TRUE][1]
+        tmpGroup$LRStartY = dat$Y[dat$IsLinear %in% TRUE][1]
         tmpGroup$LRStartX =  dat$X[dat$IsLinear %in% TRUE][1]
         tmpGroup$LREnd = data.table::last(dat$DilutionPoint[dat$IsLinear %in% TRUE])
         tmpGroup$LREndY = data.table::last(dat$Y[dat$IsLinear %in% TRUE])
@@ -274,232 +205,6 @@ findLinearRange <- function(dats, x="DilutionPoint", y = "IntensityNorm",  sd_re
 }
 
 
-
-#--------------------------------------------------------------
-
-
-
-
-# function func_lm performs linear regression for 3 consecutive points, starting at point x
-# input : data table with column names [Intensity, Concentration, ID]
-# output: tibble with informations about ID, analyzed point x, n distance and degree, and adjusted R2
-#' Title
-#'
-#' @param tbl
-#' @param x
-#' @param explanVar
-#'
-#' @return
-#' @export
-#'
-#' @examples
-func_lm <- function(tbl, x, explanVar = 'Concentration'){
-
-  tbl_linear_Range <- list()
-  lm_sum <- lm(Intensity ~ get(explanVar), data = tbl[x : (x + 2),]) %>%
-    summary()
-
-  slope <- coef(lm_sum)[2]
-  alpha.rad <- atan(slope)
-
-  alpha.deg <- 180*alpha.rad/pi
-
-
-  tibble(
-    metabolite = tbl$ID[x],
-    seqStart = tbl$DilutionPoint[x],
-    seqEnd = tbl$DilutionPoint[x + 2],
-    slope = lm_sum$coefficient[2],
-    adjr2 = lm_sum$adj.r.squared,
-    degree = alpha.deg#,
-    #indice = tbl$indice[x]
-  )
-}
-
-# function func_findLinearRange compares adjusted R2 and slope in degree to defined borders
-# input : data table from func_lm with column names [metabolite, adjR2, degree]; values for adjusted R2 and degree; Number of points necessary to consider a linear range
-# output: tibble with informations about ID, start of linear range, end of linear range, Comment
-func_findLinearRange <- function(tbl, adjR2 , degr, points){
-
-  adjr2.compare <- (round(tbl$adjr2,2) >= adjR2) & (tbl$degree >= degr)
-
-  if (!any(adjr2.compare)) { # adjusted R2 and /or slope are to small
-    tibble(
-      ID = unique(tbl$metabolite),
-      start = NA,
-      end = NA,
-      lengthRange = NA,
-      Comment = "no linear range"
-    )
-
-  }else{ # minimum of 3 consecutive points with sufficient adjusted R2 and slope
-
-
-    cons <- rle(adjr2.compare) # Compute the lengths of consecutive Trues and Falses
-    indic <- which(cons$values == TRUE & cons$lengths >= (points - 2) )
-
-    if (length(indic) > 0 ){ # length of consecutive Trues are sufficent
-      cons.lengths.cumsum.start = cumsum(cons$lengths) - cons$lengths + 1
-      cons.lengths.cumsum.end = cumsum(cons$lengths)
-      starts = tbl$seqStart[cons.lengths.cumsum.start[indic]]
-      ends = tbl$seqEnd[cons.lengths.cumsum.end[indic]]
-      maxlin  <- tbl[which(tbl[ , "degree"] == max(tbl[cons.lengths.cumsum.start[indic]:cons.lengths.cumsum.end[indic], "degree"])), ]
-      #newindex = ifelse(indic > 1, indic - 1, 0)
-      #starts = cons.lengths.cumsum[newindex] + 1
-      #if (0 %in% newindex) starts = c(1,starts)
-
-      tibble(
-        ID = unique(tbl$metabolite),
-        start = starts,
-        end = ends,
-        lengthRange = ends - starts + 1,
-        maxLinearRange = paste(maxlin[, c("seqStart", "seqEnd")], collapse = " : "),
-        maxadjR2 = max(tbl[cons.lengths.cumsum.start[indic]:cons.lengths.cumsum.end[indic], "adjr2"]),
-        Comment = ""
-      )
-    } else { # length of consecutive Trues are to small
-
-      tibble(
-        ID = unique(tbl$metabolite),
-        start = NA,
-        end = NA,
-        lengthRange = NA,
-        maxLinearRange = NA,
-        maxadjR2 = NA,
-        Comment = "no linear range"
-      )
-    }
-
-
-  }
-  #})
-}
-
-# function func_slider_LinearRange slides through data table of metabolites
-# input: data table with column names [ID, Intensity, Concentration]; Number of points necessary to consider a linear range, boundaries for adjusted R2 value and slope in degree; cooks distance
-# output:
-
-func_slider_LinearRange <- function(dat, metabolite, points = 5, adjR2 = 0.85, degr = 10, residual = 2){ #, cooksdis = 4
-
-  print(metabolite)
-  tbl_linear_Range <- list()
-  linRange <- data.frame()
-  data <- dat %>% filter(ID == metabolite) %>% arrange(Concentration) %>% mutate(DilutionPoint = 1 : nrow(.), Comment = "")
-  dataRaw <- data
-
-  #outlier
-  data <- data[!is.na(data$Intensity), ]
-  model <- lm(Intensity ~ Concentration, data)
-  data$residuals <- residuals(model)/sd(residuals(model))
-  outlier <- which(abs(data$residuals) > residual)
-
-  dataRaw <- join(type = "right", data, dataRaw)
-  dataRaw$Comment[abs(dataRaw$residuals) > residual] = "Outlier"
-  dataRaw$Comment[is.na(dataRaw$Intensity)] = "Missing"
-  tbl_linear_Range[[metabolite]][["rawData"]] <- dataRaw
-
-  if (any(outlier) | any(is.na(dataRaw$Intensity))) {
-    # outlier
-    nrOutlier <- length(outlier)
-    linRange <- tibble()
-    if (sum(outlier) > 0) data <- dataRaw[-outlier, ]
-    # NA
-    nrNA <- sum(is.na(dataRaw$Intensity))
-    NApos <- which(is.na(dataRaw$Intensity))
-    linRange <- tibble()
-    data <- data[!is.na(data$Intensity), ]
-
-  }
-
-
-
-  tbl_lm <- slide(1:(nrow(data) - 2), ~func_lm(tbl = data, .x )) %>% ldply
-
-  tbl_linear_Range[[metabolite]][["allLinearRanges"]] <- tbl_lm
-
-
-  linRange <- func_findLinearRange(tbl = tbl_lm, adjR2, degr, points )
-  linRange <- linRange %>% filter(lengthRange == max(lengthRange))
-
-  if (any(linRange$Comment == "")) { # found one linear range, calculate slope and R2 for the whole range
-    sum.all <- lm(Intensity ~ Concentration, data = dataRaw[linRange$start:linRange$end,]) %>%
-      summary()
-    alpha.rad <- atan(
-      (dataRaw$Intensity[linRange$end] - dataRaw$Intensity[linRange$start])/
-        (dataRaw$Concentration[linRange$end] - dataRaw$Concentration[linRange$start]))
-    alpha.deg <- 180*alpha.rad/pi
-
-
-    linRange$IntensityStart <- dataRaw$Intensity[linRange$start]
-    linRange$IntensityEnd <- dataRaw$Intensity[linRange$end]
-    linRange$ConcenstrationStart <- dataRaw$Concentration[linRange$start]
-    linRange$ConcenstrationEnd <- dataRaw$Concentration[linRange$end]
-    linRange$LinearRangeStart <- linRange$start
-    linRange$LinearRangeEnd <- linRange$end
-    linRange$adjr2 <- sum.all$adj.r.squared
-    linRange$degree <- alpha.deg
-    linRange$maxLinearRange <- linRange$maxLinearRange
-    linRange$maxadjR2 <- linRange$maxadjR2
-    linRange$Comment <- paste0(sum(dataRaw$Comment == "Outlier"), " outliers and ", sum(dataRaw$Comment == "Missing"), " Missings excluded")
-    linRange$PositionOfMissings <- ifelse(any(is.na(dataRaw$Intensity)),paste(dataRaw$DilutionPoint[dataRaw$Comment == "Missing"], collapse = "; "), NA)
-    linRange$PositionOfOutliers <- ifelse(any(dataRaw$Comment %in% "Outlier"),paste(dataRaw$DilutionPoint[dataRaw$Comment == "Outlier"], collapse = "; "), NA)
-    linRange$OutlierInLinearRange <- ifelse(any(dataRaw$Comment %in% "Outlier"),any(between(outlier,linRange$start, linRange$end )),NA)
-    linRange$MissingInLinearRange <- ifelse(any(is.na(dataRaw$Intensity)),any(between(NApos,linRange$start, linRange$end )), NA)
-    linRange$type <- case_when(
-      identical(sort(unique(dataRaw$Comment)), c("","Outlier")) ~ "Outlier",
-      identical(sort(unique(dataRaw$Comment)), c("","Missing")) ~ "Missing",
-      identical(sort(unique(dataRaw$Comment)), c("","Missing","Outlier")) ~ c("Outlier ,Missing"),
-      TRUE ~ "completeCase"
-    )
-
-
-  } else {# no linear range found
-    linRange <- tibble(ID = metabolite)
-
-    linRange$IntensityStart <- NA
-    linRange$IntensityEnd <- NA
-    linRange$ConcenstrationStart <- NA
-    linRange$ConcenstrationEnd <- NA
-    linRange$LinearRangeStart <- NA
-    linRange$LinearRangeEnd <- NA
-    linRange$lengthRange <- NA
-    linRange$adjr2 <- NA
-    maxLinearRange <- NA
-    linRange$degree <- NA
-    linRange$maxadjR2 <- NA
-    linRange$maxLinearRange <- NA
-    linRange$Comment <- "no linear range"
-    linRange$PositionOfMissings <- ifelse(any(is.na(dataRaw$Intensity)),paste(dataRaw$DilutionPoint[dataRaw$Comment == "Missing"]), "")
-    linRange$PositionOfOutliers <- ifelse(any(dataRaw$Comment %in% "Outlier"),paste(dataRaw$DilutionPoint[dataRaw$Comment == "Outlier"]), "")
-    linRange$OutlierInLinearRange <- NA
-    linRange$MissingInLinearRange <- NA
-    linRange$type <- case_when(
-      identical(sort(unique(dataRaw$Comment)), c("","Outlier")) ~ "Outlier",
-      identical(sort(unique(dataRaw$Comment)), c("","Missing")) ~ "Missing",
-      identical(sort(unique(dataRaw$Comment)), c("","Missing","Outlier")) ~ c("Outlier ,Missing"),
-      TRUE ~ "completeCase"
-    )
-
-  }
-
-  tbl_linear_Range[[metabolite]][["SummaryLinearRange"]] <- linRange %>% select(ID, IntensityStart, IntensityEnd, ConcenstrationStart, ConcenstrationEnd, LinearRangeStart, LinearRangeEnd, lengthRange, adjr2, degree, maxadjR2,maxLinearRange, Comment, PositionOfOutliers, OutlierInLinearRange, PositionOfMissings, MissingInLinearRange, type)
-
-  tbl_linear_Range[[metabolite]]$rawData <-
-    tbl_linear_Range[[metabolite]]$rawData %>%
-    filter(between(
-      DilutionPoint,
-      tbl_linear_Range[[metabolite]][["SummaryLinearRange"]]$LinearRangeStart,
-      tbl_linear_Range[[metabolite]][["SummaryLinearRange"]]$LinearRangeEnd)) %>%
-    mutate(Comment = replace(Comment, row_number() == 1, "start"), Comment = replace(Comment, row_number() == n(), "end")) %>%
-
-    mutate(Comment = ifelse(Comment == "", "linear", Comment)) %>%
-    rbind(tbl_linear_Range[[metabolite]]$rawData %>%
-            filter( DilutionPoint < tbl_linear_Range[[metabolite]][["SummaryLinearRange"]]$LinearRangeStart |
-                      DilutionPoint > tbl_linear_Range[[metabolite]][["SummaryLinearRange"]]$LinearRangeEnd)) %>%
-    arrange(DilutionPoint)
-
-  return(tbl_linear_Range)
-}
 
 
 
--- R/trimEnds.R
+++ R/trimEnds.R
@@ -12,18 +12,18 @@
 trimEnds <- function(dats, y="YLog", x="XLog", thresh=0){
 
   dats$trim <- FALSE
-  dats$trim[is.na(dats[, get(y)])] <- NA # | dats$OutlierFOD %in% TRUE
-  dat <- data.table::setorder(dats,DilutionPoint)[!is.na(get(y))]# & !OutlierFOD %in% TRUE]
+  dats$trim[is.na(dats[[y]])] <- NA # | dats$OutlierFOD %in% TRUE
+  dat <- data.table::setorder(dats,x)[!is.na(get(y))]# & !OutlierFOD %in% TRUE]
 
   #browser()
-  if (data.table::last(dat[[tidyselect::all_of(y)]]) != max(dat[[tidyselect::all_of(y)]])) {
+  if (data.table::last(dat[[y]]) != max(dat[[y]])) {
 
-    maxPoint <- which.max(dat[[tidyselect::all_of(y)]])
-    maxminPoint <- which(dat[[tidyselect::all_of(y)]] %in% min(dat[[tidyselect::all_of(y)]][maxPoint:length(dat[[tidyselect::all_of(y)]])]))
+    maxPoint <- which.max(dat[[y]])
+    maxminPoint <- which(dat[[y]] %in% min(dat[[y]][maxPoint:length(dat[[y]])]))
     dat.reduced.max <- data.table::copy(dat)[
-        get(tidyselect::all_of(y)) >= dat[,get(tidyselect::all_of(y))][maxminPoint],# + thresh,
-        ':=' (trim = TRUE,
-              Comment = "trim: >lastPoint")]
+      get(y) >= dat[[y]][maxminPoint],# + thresh,
+      ':=' (trim = TRUE,
+            Comment = "trim: >lastPoint")]
     # dat.reduced.max <- data.table::copy(dat)[
     #     which.max(get(tidyselect::all_of(y))) : data.table::last(get(tidyselect::all_of(y))),#get(tidyselect::all_of(y)) >= dat[,data.table::last(get(tidyselect::all_of(y)))] + thresh,
     #     ':=' (trim = TRUE,
@@ -34,27 +34,27 @@ trimEnds <- function(dats, y="YLog", x="XLog", thresh=0){
 
   } else {dat.reduced.max <- dat[, trim := FALSE]}
 
-  if (dat[[tidyselect::all_of(y)]][1] != min(dat[[tidyselect::all_of(y)]])){
+  if (dat[[y]][1] != min(dat[[y]])){
 
-    minPoint <- which.min(dat[[tidyselect::all_of(y)]])
-    minmaxPoint <- which.max(dat[[tidyselect::all_of(y)]][1:minPoint])
-    dat.reduced.min <- data.table::copy(dat)[get(tidyselect::all_of(y)) <= dat[,get(tidyselect::all_of(y))][minmaxPoint], #- thresh,
-                             ':=' (trim = TRUE,
-                                   Comment = "trim: <firstPoint")]
+    minPoint <- which.min(dat[[y]])
+    minmaxPoint <- which.max(dat[[y]][1:minPoint])
+    dat.reduced.min <- data.table::copy(dat)[get(y) <= dat[,get(y)][minmaxPoint], #- thresh,
+                                             ':=' (trim = TRUE,
+                                                   Comment = "trim: <firstPoint")]
     # dat.reduced.min <- data.table::copy(dat)[1: which.min(get(tidyselect::all_of(y))), #get(tidyselect::all_of(y)) <= dat[,get(tidyselect::all_of(y))][1] - thresh,
     #                         ':=' (trim = TRUE,
     #                               Comment = "trim: <firstPoint")]
-     dat.reduced.min[1, ':=' (Comment = "trim: firstPoint",
+    dat.reduced.min[1, ':=' (Comment = "trim: firstPoint",
                              trim = TRUE)]
 
 
 
-    } else {dat.reduced.min <- dat[, trim := FALSE]}
+  } else {dat.reduced.min <- dat[, trim := FALSE]}
 
   tmp <-  dplyr::full_join(x = dat.reduced.min[trim %in% TRUE],
-                    y = dat.reduced.max[trim %in% TRUE],
-                    by = colnames(dat.reduced.max)[colnames(dat.reduced.max) != "Comment"],
-                    suffix = c(".x", ".y")) |>
+                           y = dat.reduced.max[trim %in% TRUE],
+                           by = colnames(dat.reduced.max)[colnames(dat.reduced.max) != "Comment"],
+                           suffix = c(".x", ".y")) |>
     tidyr::unite(Comment, c(Comment.x,Comment.y), remove = TRUE, na.rm = TRUE, sep = " / ")
   data.table::setorder(tmp,DilutionPoint)
   data.table::setorder(dats,DilutionPoint)
@@ -70,3 +70,80 @@ trimEnds <- function(dats, y="YLog", x="XLog", thresh=0){
 
   return(dats)
 }
+
+
+
+#' Title
+#'
+#' @param dats
+#' @param y
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+trim_pos_associated <- function(dats, y, x, MIN_Feature, ...){
+
+  dats$trimPos <- FALSE
+  dats$trimPos[is.na(dats[[y]])] <- NA # | dats$OutlierFOD %in% TRUE
+  dat <- data.table::setorder(dats,x)[!is.na(get(y))]# & !OutlierFOD %in% TRUE]
+
+
+  IsPositivAssociated =rle(c(dat[[y]][1] < dat[[y]][2], (dat[[y]][-1] - data.table::shift(dat[[y]], 1, type = "lag")[-1]) > 0))
+  if(any(IsPositivAssociated$values) & IsPositivAssociated$lengths >= MIN_Feature){
+
+    if(length(max(IsPositivAssociated$lengths[IsPositivAssociated$values %in% TRUE])) ==1 ){
+      PA_TRUE_length_max <- max(IsPositivAssociated$lengths[IsPositivAssociated$values %in% TRUE])
+      PA_TRUE_length_max_pos <- which(IsPositivAssociated$lengths %in% PA_TRUE_length_max)
+
+      PA_TRUE_range <- c((cumsum(IsPositivAssociated$lengths)[PA_TRUE_length_max_pos-1] +1):cumsum(IsPositivAssociated$lengths)[PA_TRUE_length_max_pos])
+
+      exspected_min <- min(PA_TRUE_range)
+      exspected_max <- max(PA_TRUE_range)
+
+      if(dat[[y]][exspected_max] != data.table::last(dat[[y]]) &
+         any(dat[[y]][exspected_max : which.max(dat[[y]])] < dat[[y]][exspected_max])){
+
+        last_min <- which(dat[[y]] %in% min(dat[[y]][exspected_max:which.max(dat[[y]])]))
+        dat <- dat[get(y) >= dat[last_min, get(y)],
+                   ':=' (trimPos = TRUE,
+                         Comment = paste("trimPos",dat$Comment, sep = "_"))]
+
+      }
+
+      if(dat[[y]][exspected_min] != dat[[y]][1] &
+         any(dat[[y]][1: exspected_min] > dat[[y]][exspected_min])){
+
+        first_max <- which(dat[[y]] %in% max(dat[[y]][1: exspected_min]))
+        dat <- dat[get(y) <= dat[first_max, get(y)],
+                   ':=' (trimPos = TRUE,
+                         Comment = paste("trimPos",dat$Comment, sep = "_"))]
+
+      }
+
+
+
+    }else{
+
+      dat$Comment = paste0(dat$Comment[dat$trimPos %in% FALSE], "_Multiple.PA.ranges")
+    }
+
+
+  }
+
+  dats <- dplyr::full_join(dat, dats[!IDintern %in% dat$IDintern ], by = colnames(dats))
+
+  dats$Comment[dats$trimPos %in% FALSE] <- paste0(dats$Comment[dats$trimPos %in% FALSE], "_NoTrimPos")
+  dats$color[dats$trimPos %in% TRUE] <- "grey"
+  dats$Y_trimPos <- dats[[y]]
+  dats$Y_trimPos[dats$trimPos %in% TRUE] <- NA
+
+  return(dats)
+
+
+}
+
+
+
+
