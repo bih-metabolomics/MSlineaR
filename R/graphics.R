9a12c586d79c19ad948c581f912b6ee851ea4d57
Wiebachj <jwiebac@mdc-berlin.net>
2023-03-29 09:39:07 +0200

    add dependencies

--- R/graphics.R
+++ R/graphics.R
@@ -130,7 +130,6 @@ plot_FDS <- function(inputData_Series, inputData_BioSamples, inputData_QC, input
 
 
   nCol = data.table::uniqueN(data_Signals[,get(columns[["Batch"]])])
-  message(nrRow)
   npage = ceiling(as.numeric(data.table::uniqueN(data_Signals[[ID]])/nrRow))
 
   if(printPDF %in% TRUE){
