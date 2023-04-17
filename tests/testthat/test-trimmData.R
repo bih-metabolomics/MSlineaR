c08ca2ed6ec648746cd53f14df123c197e206459
Wiebachj <jwiebac@mdc-berlin.net>
2023-04-17 22:58:16 +0200

    adjust unit test for trimming ends

--- tests/testthat/test-trimmData.R
+++ tests/testthat/test-trimmData.R
@@ -14,11 +14,11 @@ testthat::test_that("function trimEnds works", {
 
   datOut1 <- chooseModel(dat1, y., x.,  abbr = "FOD")$`1`$dat
 
-  testthat::expect_true(all(trimEnds(dats = datOut1,y = "Y_FOD", x, thresh = 0)$trim[2:4]))
-  testthat::expect_true(all(trimEnds(datOut1,y, x, thresh = 0.5)$trim[2:3]))
-  testthat::expect_equal(trimEnds(datOut1,y = "Y_FOD", x)$trim[1], NA)
-  testthat::expect_match(trimEnds(datOut1,y = "Y_FOD", x)$Comment[2], "trim: firstPoint")
-  testthat::expect_match(trimEnds(datOut1,y = "Y_FOD", x)$Comment[3], "trim: <firstPoint")
+  testthat::expect_true(all(trimEnds(dats = datOut1,y = "Y_FOD", x., thresh = 0)$trim[2:4]))
+  testthat::expect_true(all(trimEnds(datOut1,y = "Y_FOD", x., thresh = 0.5)$trim[2:3]))
+  testthat::expect_equal(trimEnds(datOut1,y = "Y_FOD", x.)$trim[1], NA)
+  testthat::expect_match(trimEnds(datOut1,y = "Y_FOD", x.)$Comment[2], "trim: firstPoint")
+  testthat::expect_match(trimEnds(datOut1,y = "Y_FOD", x.)$Comment[3], "trim: <firstPoint")
 
 
   dat2 <- data.table::data.table(data.frame(groupIndices = rep(1,10),
@@ -33,10 +33,10 @@ testthat::test_that("function trimEnds works", {
   datOut2 <- chooseModel(dat2, y., x., abbr = "FOD")$`1`$dat
 
   testthat::expect_true(all(trimEnds(datOut2,y = "Y_FOD", x)$trim[2:4]))
-  testthat::expect_equal(trimEnds(datOut2,y = "Y_FOD", x)$trim[1], NA)
-  testthat::expect_match(trimEnds(datOut2,y = "Y_FOD", x)$Comment[2], "trim: firstPoint")
-  testthat::expect_match(trimEnds(datOut2,y = "Y_FOD", x)$Comment[3], "trim: <firstPoint")
-  testthat::expect_match(trimEnds(datOut2,y = "Y_FOD", x)$Comment[9], "trim: >lastPoint")
-  testthat::expect_match(trimEnds(datOut2,y = "Y_FOD", x)$Comment[10], "trim: lastPoint")
+  testthat::expect_equal(trimEnds(datOut2,y = "Y_FOD", x.)$trim[1], NA)
+  testthat::expect_match(trimEnds(datOut2,y = "Y_FOD", x.)$Comment[2], "trim: firstPoint")
+  testthat::expect_match(trimEnds(datOut2,y = "Y_FOD", x.)$Comment[3], "trim: <firstPoint")
+  testthat::expect_match(trimEnds(datOut2,y = "Y_FOD", x.)$Comment[9], "trim: >lastPoint")
+  testthat::expect_match(trimEnds(datOut2,y = "Y_FOD", x.)$Comment[10], "trim: lastPoint")
 
 })
