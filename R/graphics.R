58e066f63cbd2259327b3ba1b01c14099aa931ed
Wiebachj <jwiebac@mdc-berlin.net>
2023-07-24 12:45:39 +0200

    fixed bug in graphics

--- R/graphics.R
+++ R/graphics.R
@@ -175,7 +175,7 @@ plot_FDS <- function(inputData_Series, inputData_BioSamples, inputData_QC,#input
   }
 
   plotlinearData <-  plotlinearData +
-    ggplot2::scale_x_continuous(name = "Dilution", limits = c(-5, NA) ,breaks = data_Signals[[indipendent]],  labels = data_Signals$DilutionPoint +1) +#scales::trans_format(get(inverse_x), format = number_format())) +
+    ggplot2::scale_x_continuous(name = "Dilution", limits = c(-5, NA) ,breaks = data_Signals[[indipendent]],  labels = data_Signals$DilutionPoint) +#scales::trans_format(get(inverse_x), format = number_format())) +
     ggplot2::geom_vline(ggplot2::aes( xintercept = -1, color = "darkgrey"), linetype = "solid", col = "black", na.rm = TRUE)
 
 # if(!is.null(inputData_BioSamples) | !is.null(inputData_QC)| !is.null(inputData_QCref) | !is.null(inputData_Blank)){
