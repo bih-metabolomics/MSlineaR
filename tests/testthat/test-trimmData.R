00abb89872a157db5ab8388c2ff625284ccc8c75
Wiebachj <jwiebac@mdc-berlin.net>
2026-03-09 11:19:40 +0100

    update tests

--- tests/testthat/test-trimmData.R
+++ tests/testthat/test-trimmData.R
@@ -14,8 +14,8 @@ testthat::test_that("function trimEnds works", {
 
   datOut1 <- chooseModel(dat1, y., x.,  abbr = "FOD")$`1`$dat
 
-  testthat::expect_true(all(trimEnds(dats = datOut1,y = "Y_FOD", x., thresh = 0)$trim[2:4]))
-  testthat::expect_true(all(trimEnds(datOut1,y = "Y_FOD", x., thresh = 0.5)$trim[2:3]))
+  testthat::expect_true(all(trimEnds(dats = datOut1,y = "Y_FOD", x.)$trim[2:4]))
+  testthat::expect_true(all(trimEnds(datOut1,y = "Y_FOD", x.)$trim[2:3]))
   testthat::expect_equal(trimEnds(datOut1,y = "Y_FOD", x.)$trim[1], NA)
   testthat::expect_match(trimEnds(datOut1,y = "Y_FOD", x.)$Comment[2], "trim: firstPoint")
   testthat::expect_match(trimEnds(datOut1,y = "Y_FOD", x.)$Comment[3], "trim: <firstPoint")
